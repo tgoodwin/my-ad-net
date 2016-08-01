d6e50b1abacd9d23b96c9aaf15b9a205da1e264a
Timothy Goodwin <tlg2132@columbia.edu>
2016-07-31 22:36:00 -0700

    radar.js

---
+++ public/js/services/radar.js
@@ -0,0 +1,20 @@
+angular.module('radarService', [])
+
+	// super simple service
+	// each function returns a promise object 
+	.factory('Radar', ['$http',function($http) {
+		return {
+			get : function() {
+				return $http.get('/api/geo');
+			},
+			find : function() {
+				return $http.get('/api/geo/client');
+			},
+			create : function(todoData) {
+				return $http.post('/api/todos', todoData);
+			},
+			delete : function(id) {
+				return $http.delete('/api/todos/' + id);
+			}
+		}
+	}]);
