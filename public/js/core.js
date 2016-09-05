01d6b76bbd2bd2c61da5db8bd5575b6fc95d842d
Timothy Goodwin <tlg2132@columbia.edu>
2016-09-05 12:18:31 -0400

    fix split undefined

--- public/js/core.js
+++ public/js/core.js
@@ -21,8 +21,10 @@ app.directive('superMap', ['topo', function(topo) {
 
 			var project = function(d) {
 				//coords stored as lat,long
-				var coords = d.coordinate.split(',');
-				return projection([+coords[1], +coords[0]]);
+				if(!!d == true) {
+					var coords = d.coordinate.split(',');
+					return projection([+coords[1], +coords[0]]);
+				}
 			};
 
 			var size = function() {
